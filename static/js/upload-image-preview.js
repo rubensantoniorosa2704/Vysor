c876971e6b8be4a3664eef3196b74ca8a2484539
rubensantoniorosa2704 <rubensrosaneto27@gmail.com>
2024-03-29 22:15:45 -0300

    refactor upload-image-preview.js using arrow functions instead of old functions

--- static/js/upload-image-preview.js
+++ static/js/upload-image-preview.js
@@ -1,8 +1,8 @@
 // Function to preview the selected image
-function previewImage(event) {
-    let reader = new FileReader();
-    reader.onload = function(){
-        let output = document.getElementById('image-preview');
+const previewImage = (event) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+        const output = document.getElementById('image-preview');
         output.src = reader.result;
         output.style.display = 'block';
         sessionStorage.setItem('uploaded-image', reader.result);
@@ -11,23 +11,20 @@ function previewImage(event) {
     reader.readAsDataURL(event.target.files[0]);
 }
 
-// When the page is loaded, check if there is an image in the localStorage
-window.onload = function() {
-    // Retrieve the uploaded image from localStorage
-    let uploadedImage = sessionStorage.getItem('uploaded-image');
-    // If an image is found in localStorage
+
+// When the page is loaded, check if there is an image in the sessionStorage
+window.onload = () => {
+    const uploadedImage = sessionStorage.getItem('uploaded-image');
     if (uploadedImage) {
-        // Get the image preview element
-        let output = document.getElementById('image-preview');
-        // Set the source of the image to the one stored in localStorage
+        const output = document.getElementById('image-preview');
         output.src = uploadedImage;
-        // Make the image preview visible by changing its display style to 'block'
         output.style.display = 'block';
     }
 }
 
+
 // Clears sessionStorage when the page is reloaded
-window.addEventListener('beforeunload', function() {
+window.addEventListener('beforeunload', () => {
     // Clear session storage
     sessionStorage.clear();
 });
