63bacb65e0977ce7235a48d42e8f05b7a652cdab
rubensantoniorosa2704 <rubensrosaneto27@gmail.com>
2024-04-22 21:45:23 -0300

    New TTS button

--- static/js/response_notification.js
+++ static/js/response_notification.js
@@ -1,11 +1,14 @@
-/*
-* Hides the message if the X is pressed
-*
-* const closeMessage = () => {
-*     document.querySelector('#response-section').style.display = 'none';
-* };
-*
-* This section was deactivated because the section container
-* now uses the modal element from the Bulma framework. This code
-* will remain here anyway for register purposes.
-*/
+// Define an arrow function for the TTS action
+const playTextToSpeech = () => {
+    const description = document.querySelector('#description').innerText;
+
+    // Create a new instance of SpeechSynthesisUtterance
+    const msg = new SpeechSynthesisUtterance(description);
+
+    // Set additional properties (if needed)
+    msg.lang = 'en-US'; // Set the language (e.g., 'en-US' for United States English)
+    msg.rate = 1.0; // Set the speech rate (1.0 is the default)
+
+    // Start speech synthesis
+    window.speechSynthesis.speak(msg);
+};
